94577ff0e3e6f4dd64297ddb9520dac5f0524f14
moradaniel <moradaniel@gmail.com>
2019-06-08 12:23:40 -0300

    Added Content-Type header

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -1,4 +1,4 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
@@ -11,11 +11,16 @@ export class AuthInterceptor implements HttpInterceptor {
 
     constructor(private token: TokenStorageService) { }
 
+
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         let authReq = req;
         const token = this.token.getToken();
         if (token != null) {
-            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+            authReq = req.clone({
+                        headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token)
+                                  .set('Content-Type', 'application/json; charset=utf-8')
+                                  .set('Accept', 'application/json')
+              });
         }
         return next.handle(authReq);
     }
